b87e3234481265619bfbff0fa2e017a943f9b0b1
Spector323 <amiraldiev752@gmail.com>
2025-06-17 12:09:35 +0300

    commit 3

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -5,17 +5,36 @@ export default function Header() {
     return (
         <header className="header">
             <div className="cn-header">
-                <img src="./logo.svg" alt="" className="header-logo" />
+                <a href="/">
+                    <img src="./logo.svg" alt="" className="header-logo" />
+                </a>
+
                 <div className="header-nav">
                     <a href="">Main Page</a>
                     <a href="">Categories</a>
                     <a href="">All products</a>
                     <a href="">All sales</a>
                 </div>
-                <div className="header-basket">
-                    <img src="./assets/icons/basket.svg" alt="" />
-                </div>
+
+                <a href="/">
+                    <img src="./assets/icons/basket.svg" alt="" className="header-basket" />
+                </a>
+
             </div>
+
+            <nav className="nav">
+                <div className="block-nav">
+                    <div className="cn-bl-nav">
+                        <h1>Amazing Discounts <br />
+                             on Garden Products!
+                        </h1>
+                        <a href="/products">
+                            <button className="btn-bl-nav">Check out</button>
+                        </a>
+                    </div>
+                </div>
+            </nav>
         </header>
+
     )
 }
