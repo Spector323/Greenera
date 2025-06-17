0dab8ecc63fae8d6a774a8eb1988628f2322099f
Spector323 <amiraldiev752@gmail.com>
2025-06-17 10:27:19 +0300

    commit 1

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,14 @@
+export default function Header(){
+    return(
+        <header className="header">
+                <div className="cn-header">
+                    <div className="logo"></div>
+                    <div className="header-nav"></div>
+                    <div className="header-basket">
+                        <img src="./assets/icons/basket.svg" alt="" />
+                        hfkyg
+                    </div>
+                </div>
+        </header>
+    )
+}
