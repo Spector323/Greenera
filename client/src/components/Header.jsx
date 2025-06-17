381ad6141dfb7023a704b0259fdc9b19c0c2f0cd
Spector323 <amiraldiev752@gmail.com>
2025-06-17 11:11:41 +0300

    commit 2

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,14 +1,21 @@
-export default function Header(){
-    return(
+import "../styles/header.css"
+
+
+export default function Header() {
+    return (
         <header className="header">
-                <div className="cn-header">
-                    <div className="logo"></div>
-                    <div className="header-nav"></div>
-                    <div className="header-basket">
-                        <img src="./assets/icons/basket.svg" alt="" />
-                        hfkyg
-                    </div>
+            <div className="cn-header">
+                <img src="./logo.svg" alt="" className="header-logo" />
+                <div className="header-nav">
+                    <a href="">Main Page</a>
+                    <a href="">Categories</a>
+                    <a href="">All products</a>
+                    <a href="">All sales</a>
                 </div>
+                <div className="header-basket">
+                    <img src="./assets/icons/basket.svg" alt="" />
+                </div>
+            </div>
         </header>
     )
 }
