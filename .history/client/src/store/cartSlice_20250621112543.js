2cfaaa0e374a40c610e2b0d3ad54674f3fe73c06
Spector <amiraldiev752@gmail.com>
2025-06-21 18:14:41 +0300

    commit 11

---
+++ .history/client/src/store/cartSlice_20250621112543.js
@@ -0,0 +1,23 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const cartSlice = createSlice({
+  name: 'cart',
+  initialState: [],
+  reducers: {
+    addToCart: (state, action) => {
+      const product = action.payload;
+      const existingProduct = state.find((item) => item.id === product.id);
+      if (existingProduct) {
+        existingProduct.quantity += 1;
+      } else {
+        state.push({ ...product, quantity: 1 });
+      }
+    },
+    removeFromCart: (state, action) => {
+      return state.filter((item) => item.id !== action.payload);
+    },
+  },
+});
+
+export const { addToCart, removeFromCart } = cartSlice.actions;
+export default cartSlice.reducer;
